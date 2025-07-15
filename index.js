2d0a0f1c567bd449f0bf2f9df23909b6be58f87e
Typlosion Flamme <pro-morganhue@outlook.fr>
2025-07-15 21:37:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const { writeFileSync } = require('fs');
+var convert = require('xml-js');
+const { program } = require('commander');
+program
+  .option('-x, --xml <xmlFile>','Xml File to found modelName','xml.xml')
+  .option('-o, --out <outFile>','Txt File to put the list',`${Date.now()}.txt`);
+
+program.parse();
+
+const options = program.opts();
+var xml = require('fs').readFileSync('./'+ options.xml, 'utf8');
+var result = JSON.parse(convert.xml2json(xml, {compact: true, spaces: 4}));
+
+// writeFileSync('xml.json', result)
+let textContent = ""
+result.CVehicleModelInfo__InitDataList.InitDatas.Item.forEach(element => {
+    textContent = textContent + `${element.modelName._text}\n`
+});
+writeFileSync(options.out, textContent)
